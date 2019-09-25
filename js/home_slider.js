939c3c0839251828449abb8535a2e80cf8242cb9
Sean Park <seanpark1107@gmail.com>
2019-09-25 15:25:41 -0400

    added slider next button

--- js/home_slider.js
+++ js/home_slider.js
@@ -2,6 +2,7 @@
 
 let transform = $('#banner-slider-slides').css('translateX');
 var currentSlide = 0;
+var numSlides = 3;
 
 // console.log($('.slider-dot').get(0));
 let firstDot = $('.slider-dot').get(0);
@@ -9,12 +10,22 @@ $(firstDot).addClass('active');
 
 $('.slider-dot').click(function() {
 	let index = $('.slider-dot').index($(this));
-	let slideAmount = index * -100;
+	moveSlider(index);
+});
+
+$('#home-banner-next-button').click(function(e){
+	e.preventDefault();
+	let newIndex = (currentSlide + 1) % numSlides;
+	moveSlider(newIndex);
+});
+
+function moveSlider(newIndex) {
+	currentSlide = newIndex;
+	let slideAmount = newIndex * -100;
 	let transformProp = 'translateX(' + slideAmount + 'vw)';
 	$('#banner-slider-slides').css('transform', transformProp);
 
 	$('.slider-dot').removeClass('active');
-	let activeDot = $('.slider-dot').get(index);
+	let activeDot = $('.slider-dot').get(newIndex);
 	$(activeDot).addClass('active');
-
-});
+}
