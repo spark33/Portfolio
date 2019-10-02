67923ba77863135cda7ce1ae1b6cf0103a038024
Sean Park <seanpark1107@gmail.com>
2019-10-02 14:47:01 -0400

    added scroll animation for projects

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -4,3 +4,19 @@ $('.nav-toggle').click(function() {
 	$('body').toggleClass('nav-open');
 	$('#mobile-nav-screen').toggleClass('visible');
 });
+
+// Scroll to home section
+
+$('a[href="#projects-section"]').click(function(e) {
+	e.preventDefault();
+	let page = "/" + $(this).attr('href').split('#')[0];
+	let hash = '#' + $(this).attr('href').split('#')[1]
+	console.log(hash);
+	
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 750);
+	location.href.replace(hash, "");
+	$('body').removeClass('nav-open');
+	$('#mobile-nav-screen').removeClass('visible');
+});
