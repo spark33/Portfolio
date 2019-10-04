3d6fc13cf1a4c65dd326eccb0e4a4013a753d8c0
Sean Park <seanpark1107@gmail.com>
2019-10-04 11:14:36 -0400

    changed loader times

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,9 @@
 $(window).on("load",function(){
 	setTimeout(function() {
 		$('.loader-wrapper > h4').addClass('active');
-	}, 3000);
+	}, 1000);
 	setTimeout(function() {
 		$(".loader-wrapper").addClass('hidden');
 		$("#default-layout-main-site").css('display', 'block');
-	}, 4000);
+	}, 2500);
 });
