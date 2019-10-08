7123bac57ed8c59e7228f26da5654ac6abd12763
Sean Park <seanpark1107@gmail.com>
2019-10-08 16:10:28 -0500

    better ie checker

--- js/loader.js
+++ js/loader.js
@@ -7,3 +7,21 @@ $(window).on("load",function(){
 		$("#default-layout-main-site").css('display', 'block');
 	}, 3000);
 });
+
+$(document).ready(msieversion);
+
+function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
+    {
+        let ieMessage = "Hey! Thanks for visiting. Unfortunately, my site doesn't currently support Internet Explorer. Try me in another browser!"
+	    $('.loader-wrapper').append(ieMessage);
+
+	    window.stop();
+    }
+
+    return false;
+}
